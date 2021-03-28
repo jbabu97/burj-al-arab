f641a8103eea708a32c0d3a764548978d86b71c9
Jabbar Babu <jabbar.babu81@gmail.com>
2021-03-28 11:07:00 +0600

    first commit

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDjQ1zzJDPnAg9i8BEa5NIkkhqeimYwIPo",
+  authDomain: "burz-jb.firebaseapp.com",
+  projectId: "burz-jb",
+  storageBucket: "burz-jb.appspot.com",
+  messagingSenderId: "310494074814",
+  appId: "1:310494074814:web:3af585acab686d76694bef"
+};
+
+  export default firebaseConfig;
